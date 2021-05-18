11a2943b3154d4c0e330de966f99fe3a2b878ee9
Jhon <jhonhuertas96@gmail.com>
2021-05-18 00:07:20 -0500

    Registro ultima posicion, y caso r agregado

--- ProyectoC++/map.cpp
+++ ProyectoC++/map.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+
+
+void SaveLocation(int HeroPosX, int HeroPosY)
+{
+    ofstream MyFile("GameData.txt");
+    string location = "";
+    if (MyFile.is_open())
+    {
+        MyFile << "Ubicacion actual: " << endl;
+        for (int i = HeroPosX; i > 0; i--)
+        {
+            MyFile << "r" << endl;
+        }
+        for (int i = HeroPosY; i > 0; i--)
+        {
+            MyFile << "c" << endl;
+        }
+
+    }
+
+    MyFile.close();
+}
 
 void DrawMap(int HeroPosX, int HeroPosY, char GameMap[5][5])
 {
@@ -16,7 +39,8 @@ void DrawMap(int HeroPosX, int HeroPosY, char GameMap[5][5])
                 cout << GameMap[i][p];
         }
         cout << endl;
-    }
+        SaveLocation(HeroPosX, HeroPosY);
+    }   
 }
 
 int main()
@@ -33,6 +57,30 @@ int main()
         {'1', '1', '1', '1', '1'},
         {'1', '1', '1', '1', '1'}
     };    
+
+    ifstream MyFileRead("GameData.txt");
+    string line;
+    string NombreDelHeroe = "";
+    int renglon = 0;
+
+    if (MyFileRead.is_open())
+    {
+        while (getline(MyFileRead, line))
+        {
+            renglon = renglon + 1;
+            if (renglon > 1)
+            {
+                if (line == "r")
+                    HeroPosX = HeroPosX + 1;
+                if (line == "c")
+                    HeroPosY = HeroPosY + 1;
+            }
+        }    
+    }
+    else
+    {
+        cout << "No logre abrir mi archivo, checar el antivirus!!" << endl;
+    }
 
     DrawMap(HeroPosX, HeroPosY, GameMap);
 
@@ -59,7 +107,13 @@ int main()
                 HeroPosX = HeroPosX +1;
             break;
         default:
-            isGameOver = true;
+            if (Input == 'r')
+            {
+                HeroPosX = 0;
+                HeroPosY = 0;
+            }
+            else
+                isGameOver = true;
             break;
         }
         DrawMap(HeroPosX, HeroPosY, GameMap);
